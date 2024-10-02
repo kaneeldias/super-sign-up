2ef5f797651ead709b4c2c5623250f16909e357c
kaneeldias <kaneeldias@hotmail.com>
2024-10-02 14:36:36 -0400

    Add campaign tag to force sign up

--- src/utils/auth-client-utils.ts
+++ src/utils/auth-client-utils.ts
@@ -1,5 +1,5 @@
 export const OAUTH_ENDPOINT = `${process.env.NEXT_PUBLIC_AUTH_ENDPOINT}/oauth/authorize`;
 
 export function getOauthUrl() {
-    return `${OAUTH_ENDPOINT}?client_id=${process.env.NEXT_PUBLIC_AUTH_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_AUTH_REDIRECT_URI as string)}&response_type=code`;
+    return `${OAUTH_ENDPOINT}?client_id=${process.env.NEXT_PUBLIC_AUTH_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.NEXT_PUBLIC_AUTH_REDIRECT_URI as string)}&response_type=code&utm_source=Website&utm_medium=Stories&utm_campaign=MLT-SuperSignup&utm_term=SS&utm_content=General`;
 }
